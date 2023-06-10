ff13d856e09a1018b7cb44c95e8be94e0f1392fe
Tatiana <tatiana.martinez.castrillon@gmail.com>
2023-06-09 20:54:15 -0500

    Ciclos For-if-While-Break-Anidacion

---
+++ ExerciseJava_ONE/src/ExerciseJava/SumaContador.java
@@ -0,0 +1,20 @@
+package ExerciseJava;
+
+public class SumaContador {
+	public static void main (String[] args) {
+
+        int contador = 1;
+        int total=0;
+
+        while (contador <= 10) {
+        	total=total+contador;
+            System.out.println (total); 
+            contador ++;
+        }
+
+
+
+    }
+
+
+}
