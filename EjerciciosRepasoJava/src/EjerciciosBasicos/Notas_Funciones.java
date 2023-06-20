52b8bb53770850a1e1c08b908b3debb8a16777d4
Tatiana <tatiana.martinez.castrillon@gmail.com>
2023-06-20 16:31:27 -0500

    "NotasEstudiantes-Funciones"

--- EjerciciosRepasoJava/src/EjerciciosBasicos/Notas_Funciones.java
+++ EjerciciosRepasoJava/src/EjerciciosBasicos/Notas_Funciones.java
@@ -5,26 +5,29 @@ import java.util.Scanner;
 public class Notas_Funciones {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		System.out.println("Cuantos estudiantes desea ingresar");
+		int estudiantes = in.nextInt();
 		System.out.println("Digite cuantas notas desea ingresar");
 		int tamano = in.nextInt();
 		// Crear el vector
 		float[] notasEstudiante = new float[tamano];
 
 		// Leer las notas del estudiante
-		for (int i = 0; i < tamano; i++) {
-			System.out.print("Ingrese la nota " + (i + 1) + ": ");
-			notasEstudiante[i] = in.nextFloat();
-		}
-		// Imprimir las notas del estudiante
-		System.out.println("Notas ingresadas del estudiante:");
-		for (int i = 0; i < tamano; i++) {
-			System.out.println("Nota " + (i + 1) + ": " + notasEstudiante[i]);
-		}
+		for (int j = 0; j < estudiantes; j++) {
+			float promedio;
 
-		// Calcular el promedio de las notas
-		float promedio = obtenerPromedio(notasEstudiante);
+			System.out.println("Estudiante" + (j + 1));
+			for (int i = 0; i < tamano; i++) {
+				System.out.print("Ingrese la nota " + (i + 1) + ": ");
+				notasEstudiante[i] = in.nextFloat();
+			}
+			promedio = obtenerPromedio(notasEstudiante);
+			System.out.println("El promedio de las notas del estudiante " + (j + 1) + " es: " + promedio);
 
-		System.out.println("El promedio de las notas es: " + promedio);
+			System.out.println("Las notas del estudiante " + (j + 1) + " son:");
+			imprimirNotas(notasEstudiante);
+
+		}
 
 		in.close();
 	}
@@ -32,11 +35,17 @@ public class Notas_Funciones {
 	// Función para obtener el promedio de un vector de notas
 	public static float obtenerPromedio(float[] notasEstudiante) {
 		float suma = 0;
-
 		for (float nota : notasEstudiante) {
 			suma += nota;
 		}
-
 		return suma / notasEstudiante.length;
+	}
+
+	public static void imprimirNotas(float[] notasEstudiantes) {
+
+		for (float nota : notasEstudiantes) {
+			System.out.println("Nota : " + nota);
+
+		}
 	}
 }
