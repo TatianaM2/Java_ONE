0000e174f41ce53bbf96e4362bc763718d92235c
Tatiana <tatiana.martinez.castrillon@gmail.com>
2023-06-19 22:12:05 -0500

    "Condicional For"

---
+++ EjerciciosRepasoJava/src/EjerciciosBasicos/Condicional_For.java
@@ -0,0 +1,14 @@
+package EjerciciosBasicos;
+
+public class Condicional_For {
+	public static void main(String[] args) {
+		int [] numeros= {1, 5, 7, 9, 10, 11};
+		System.out.println("En total son "+numeros.length+" numeros");
+		for(int i =0; i<numeros.length; i++) {
+			System.out.println("Los numeros  son "+numeros[i]);
+			
+			
+		}
+	}
+
+}
