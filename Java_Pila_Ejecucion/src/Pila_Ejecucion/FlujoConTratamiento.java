30570f65315d1b63a37d53669d2bf5c4e3d27aaf
Tatiana <tatiana.martinez.castrillon@gmail.com>
2023-07-15 21:10:14 -0500

    "Checked y Unchecked"

--- Java_Pila_Ejecucion/src/Pila_Ejecucion/FlujoConTratamiento.java
+++ Java_Pila_Ejecucion/src/Pila_Ejecucion/FlujoConTratamiento.java
@@ -6,23 +6,27 @@ public class FlujoConTratamiento {
 
 	public static void main(String[] args) {
         System.out.println("Inicio main");
-        metodo1();
+        try {
+            metodo1();
+    } catch(ArithmeticException | NullPointerException | MiExcepcion ex) {
+            String msg = ex.getMessage();
+
+            System.out.println("Exception " + msg);
+            ex.printStackTrace();
+    }
         System.out.println("Fin de main");
     }
 
-    private static void metodo1() {
-        System.out.println("Inicio de metodo1");
-        try {
-            metodo2();
-        } catch(ArithmeticException ex) {
-            System.out.println("ArithmeticException");
-        }
-        System.out.println("Fin de metodo1");
+    private static void metodo1() throws MiExcepcion {
+        System.out.println("Inicio del metodo1");
+        metodo2();
+        System.out.println("Fin del metodo1");
     }
 
-    private static void metodo2() {
-    	System.out.println("Inicio del metodo2");
-        throw new ArithmeticException("Surgió un error");
+    private static void metodo2() throws MiExcepcion {
+        System.out.println("Inicio del metodo2");
+        throw new MiExcepcion ("Surgió un error");
+        // System.out.println("Fin del metodo2"); 
     }
 
 	
